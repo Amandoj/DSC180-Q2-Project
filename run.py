c7913415ac4c3e5a02a2e155c9d6e15308d67643
Amando Jimenez <57554350+Amandoj@users.noreply.github.com>
2023-01-22 13:14:29 -0800

    init

---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+## NECESSARY IMPORTS
+import sys
+import json
+import os
+import shutil
+
+# from src.data import make_dataset
+# from src.features import build_features
+# from src.models import train_model
+# from src.visualization import visualize
