12a74da135843a7919a778bd1c78ae977a76932d
Amando Jimenez <57554350+Amandoj@users.noreply.github.com>
2023-02-07 19:18:19 -0800

    making ml python scripts

--- src/models/make_models.py
+++ src/models/make_models.py
@@ -2,3 +2,12 @@ from qiime2.plugins.sample_classifier.pipelines import classify_samples
 
 def sample_classifier_single_disease(feature_table, metadataCol):
     return classify_samples(feature_table, metadataCol, missing_samples='ignore')
+
+def sample_classifier_diseases(feature_table, metadata, disease_targets):
+    disease_cols = [metadata.get_column(disease) for disease in disease_targets]
+    results = []
+    for metadata_disease_col in disease_cols:
+        qiime_model = sample_classifier_single_disease(feature_table, metadata_disease_col)
+        results.append(qiime_model)
+    return results
+    
--- src/visualizations/make_visualizations.py
+++ src/visualizations/make_visualizations.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+def create_bar_col_binary(df, col_name):
+    ax = df[col_name].value_counts().sort_index(ascending=False).plot(kind='barh')
+    ax.set_xlabel('count')
+    ax.set_ylabel('outcome')
+    ax.set_title(col_name)
+    
+def disease_counts_graph(metadata, disease_cols):
+    metadata[disease_cols].sum().sort_values(ascending=False).plot(kind='bar')
