d1771e69b69a8bf995dd5471b7e4ac35e676417e
Amando Jimenez <57554350+Amandoj@users.noreply.github.com>
2023-03-12 19:12:18 -0700

    updating documentation

--- src/features/metrics_analysis.py
+++ src/features/metrics_analysis.py
@@ -3,16 +3,41 @@ from qiime2.plugins.diversity.visualizers import beta_group_significance
 
 
 def calculate_unifrac_distance_matrices(feature_table, phylogeny):
+    """Calculate weighted and unweighted unifrac distance matrices
+
+    Args:
+        feature_table (FeatureTable[Frequency]): Feature table that will be used to calculate distance matrix
+        phylogeny (Phylogeny[Rooted]): Tree artifact
+
+    Returns:
+        DistanceMatrix, DistanceMatrix: unweighted and weighted unifrac distance matrices
+    """
     distance_matrices = ['unweighted_unifrac','weighted_unifrac']
     u_unifrac_dis_matrix = beta_phylogenetic(feature_table, phylogeny, 'unweighted_unifrac').distance_matrix
     w_unifrac_dis_matrix = beta_phylogenetic(feature_table, phylogeny, 'weighted_unifrac').distance_matrix
     return u_unifrac_dis_matrix, w_unifrac_dis_matrix
 
 def calculate_distance_matrix(feature_table, metric):
+    """Calculate distance matrix based on given metric, no phylogeny tree necessary
+
+    Args:
+        feature_table (FeatureTable[Frequency]): Feature table that will be used to calculate distance matrix
+        metric (String): The beta diversity metric to be computed.
+
+    Returns:
+        DistanceMatrix: Distance matrix based on feature table
+    """
     return beta(feature_table, metric).distance_matrix
         
 
 def permanova_test(distance_matrix, metadata_col, metric):
+    """Perform permanova test on given column
+
+    Args:
+        distance_matrix (DistanceMatrix): Distance Matrix
+        metadata_col (_type_): Column that will undergo permanova test
+        metric (String): The beta diversity metric to be computed.
+    """
     permanova_result = beta_group_significance(distance_matrix, metadata_col, method='permanova')
     permanova_result.visualization.save('data/out/'+metric+'_permanova_test_'+metadata_col.name)
     
