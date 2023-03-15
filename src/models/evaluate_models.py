c0aa6205b1431ce54b2da6e1eeb1a39b5c8a7791
Amando Jimenez <57554350+Amandoj@users.noreply.github.com>
2023-03-14 17:14:34 -0700

    deleting typo

--- src/models/evaluate_models.py
+++ src/models/evaluate_models.py
@@ -14,7 +14,6 @@ def binary_relevance_accuracy_scores(model_results, disease_targets):
     """
     disease_accuracy_scores = {}
     for i in disease_targets:
-        ## Maybe can include auc in this
         score = accuracy_score(model_results[i].test_targets.view(pd.Series), model_results[i].predictions.view(pd.Series))
         disease_accuracy_scores[i] = score
         
